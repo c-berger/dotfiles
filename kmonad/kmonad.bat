1488100780c6691876f0b6c2f1d94ca9be5e20cb
C. Berger <71664347+c-berger@users.noreply.github.com>
2022-10-07 09:45:52 +0200

    Update Kmonad batch files.

--- kmonad/kmonad.bat
+++
@@ -1,8 +0,0 @@
-@ECHO off
-
-
-@REM kmonad.exe %~dp0\config.kbd
-
-@REM requires nircmd installed
-@REM use `scoop install nircmd`
-nircmd exec hide kmonad.exe %~dp0\config.kbd
---
+++ kmonad/kmonad_start.bat
@@ -0,0 +1,2 @@
+nircmd execmd "kmonad.exe --log-level debug %~dp0\config.kbd > %~dp0\kmonad.log"
+
