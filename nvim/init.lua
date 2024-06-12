e48b96a6f33e50c8e800bd3d41746c1838f014bf
C. Berger <71664347+c-berger@users.noreply.github.com>
2024-06-12 15:59:53 +0200

    Neovim: starting to migrate to lazy-nvim from packer.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,24 +1,4 @@
-if vim.g.vscode then
-    -- VSCode extension
-    print("VSCode-NeoVim init")
-else
-    -- ordinary Neovim
-    require("user.plugins")
-    require("user.core.keymaps")
-    require("user.core.options")
-    require("user.core.colorscheme")
-    require("user.plugins.comment")
-    require("user.plugins.lualine")
-    require("user.plugins.bufferline")
-    require("user.plugins.which-key")
-    require("user.plugins.gitsigns")
-    require("user.plugins.nvim-tree")
-    require("user.plugins.nvim-cmp")
-    require("user.plugins.treesitter")
-    require("user.plugins.lsp.mason")
-    require("user.plugins.telescope")
-    require("user.plugins.codeium")
-    require("user.plugins.lsp.lspsaga")
-    require("user.plugins.lsp.lspconfig")
-    require("user.plugins.lsp.null-ls")
-end
+require("user.core.keymaps")
+require("user.core.options")
+require("user.plugins")
+require("user.core.colorscheme")
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,118 +1,106 @@
--- auto-install Packer if not installed yet
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+-- auto-install Lazy.nvim if not installed yet
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
-local packer_bootstrap = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
 -- autocommand that reloads neovim and installs/updates/removes plugins
 -- when file is saved
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+-- vim.cmd([[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]])
 
--- import packer safely
-local status, packer = pcall(require, "packer")
-if not status then
-	return
-end
-
-return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+return require("lazy").setup({
 
 	-- colorschemes
-	use("tomasiser/vim-code-dark")
-	use("Mofiqul/vscode.nvim")
-	use("bluz71/vim-nightfly-colors")
-	use("bluz71/vim-moonfly-colors")
-	use("Th3Whit3Wolf/one-nvim")
-	use("ellisonleao/gruvbox.nvim")
-	use("xiyaowong/transparent.nvim")
-	use("navarasu/onedark.nvim")
+	"tomasiser/vim-code-dark",
+	"Mofiqul/vscode.nvim",
+	"bluz71/vim-nightfly-colors",
+	"bluz71/vim-moonfly-colors",
+	"Th3Whit3Wolf/one-nvim",
+	"ellisonleao/gruvbox.nvim",
+	"xiyaowong/transparent.nvim",
+	"navarasu/onedark.nvim",
 
 	-- essential plugins
-	use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
-	use("tpope/vim-repeat") -- fix "." for pluging mappings
-	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
-	use("szw/vim-maximizer") -- maximizes and restores current window
-	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
-	use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
-	use("numToStr/Comment.nvim") -- comment/uncomment
-	use("famiu/bufdelete.nvim") -- properly delete buffers without closing windows
-	use("folke/which-key.nvim") -- popup for keybindins
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"tpope/vim-repeat", -- fix "." for pluging mappings
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"szw/vim-maximizer", -- maximizes and restores current window
+	"tpope/vim-surround", -- add, delete, change surroundings (it's awesome)
+	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+	"numToStr/Comment.nvim", -- comment/uncomment
+	"famiu/bufdelete.nvim", -- properly delete buffers without closing windows
+	"folke/which-key.nvim", -- popup for keybindins
 
 	-- file explorer
-	use("nvim-tree/nvim-tree.lua")
+	"nvim-tree/nvim-tree.lua",
 
 	-- vs-code like icons
-	use("nvim-tree/nvim-web-devicons")
+	"nvim-tree/nvim-web-devicons",
 
 	-- statusline / bufferline
-	use("nvim-lualine/lualine.nvim")
-	use({ "akinsho/bufferline.nvim", tag = "*", requires = "nvim-tree/nvim-web-devicons" })
+	"nvim-lualine/lualine.nvim",
+	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 
 	-- fuzzy finding w/ telescope
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { { "nvim-lua/plenary.nvim" } } }) -- fuzzy finder
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- dependency for better sorting performance
+	{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { { "nvim-lua/plenary.nvim" } } }, -- fuzzy finder
 
 	-- autocompletion
-	use("hrsh7th/nvim-cmp") -- completion plugin
-	use("hrsh7th/cmp-buffer") -- source for text in buffer
-	use("hrsh7th/cmp-path") -- source for file system paths
+	"hrsh7th/nvim-cmp", -- completion plugin
+	"hrsh7th/cmp-buffer", -- source for text in buffer
+	"hrsh7th/cmp-path", -- source for file system paths
 
 	-- snippets
-	use("L3MON4D3/LuaSnip") -- snippet engine
-	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-	use("rafamadriz/friendly-snippets") -- useful snippets
+	"L3MON4D3/LuaSnip", -- snippet engine
+	"saadparwaiz1/cmp_luasnip", -- for autocompletion
+	"rafamadriz/friendly-snippets", -- useful snippets
 
 	-- managing & installing lsp servers, linters & formatters
-	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+	"williamboman/mason.nvim", -- in charge of managing lsp servers, linters & formatters
+	"williamboman/mason-lspconfig.nvim", -- bridges gap b/w mason & lspconfig
 
 	-- configuring lsp servers, linters and formatters
-	use("neovim/nvim-lspconfig") -- easily configure language servers
-	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
-	use("kkharji/lspsaga.nvim") -- enhanced lsp uis
+	"neovim/nvim-lspconfig", -- easily configure language servers
+	"hrsh7th/cmp-nvim-lsp", -- for autocompletion
+	"onsails/lspkind.nvim", -- vs-code like icons for autocompletion
+	"kkharji/lspsaga.nvim", -- enhanced lsp uis
 
 	-- formatting & linting
-	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	"jose-elias-alvarez/null-ls.nvim", -- configure formatters & linters
+	"jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
 
 	-- treesitter configuration
-	use({
+	{
 		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+		build = function()
+			local ts_update = dependencies("nvim-treesitter.install").update({ with_sync = true })
 			ts_update()
 		end,
-	})
+	},
 
-	-- git integration
-	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+	-- git integratiotagn
+	"lewis6991/gitsigns.nvim", -- show line modifications on left hand side
 
 	-- Codeium
-	use({
+	{
 		"Exafunction/codeium.nvim",
-		requires = {
+		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"hrsh7th/nvim-cmp",
 		},
-	}) -- AI plugin by codeium
+	}, -- AI plugin by codeium
+})
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if packer_bootstrap then
-		require("packer").sync()
-	end
-end)
