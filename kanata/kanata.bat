d04ed8761918d17733ea6a522a7c5fbb73e289ac
C. Berger <71664347+c-berger@users.noreply.github.com>
2024-06-19 15:46:47 +0200

    Kanata: update files.

--- kanata/kanata.bat
+++ kanata/kanata.bat
@@ -8,3 +8,16 @@ set KANATA=%USERPROFILE%\kanata\target\debug\kanata.exe
 @REM requires nircmd installed
 @REM use `scoop install nircmd`
 nircmd exec hide %KANATA% --cfg %~dp0\config.kbd
+
+@SET KANATA=%USERPROFILE%\kanata\target\debug\kanata.exe
+@SET CONFIG=%~pd0\config.kbd
+@SET LOGFLE=%~pd0\kanata.log
+
+@REM Kill any existing process before starting a new one.
+@REM taskkill /f /im kanata.exe
+
+@REM Execute Kanata in background and set priority to realtime.
+@REM nircmd execmd "%KANATA% --trace --cfg %CONFIG% > %LOGFLE% 2>&1"
+@REM nircmd setprocesspriority kanata.exe realtime
+
+%KANATA% --trace --cfg %CONFIG% > %LOGFLE% 2>&1
