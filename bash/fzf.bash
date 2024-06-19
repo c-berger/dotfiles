23a545fbb526abe4a18aa0b4fe3546fc7cc61997
C. Berger <71664347+c-berger@users.noreply.github.com>
2024-06-19 12:30:41 +0200

    FZF: Update default command to include "ignored" files.

--- bash/fzf.bash
+++ bash/fzf.bash
@@ -14,6 +14,6 @@ source "/home/chberger/.fzf/shell/key-bindings.bash"
 
 # Setup with fd-find
 # ------------------
-export FZF_DEFAULT_COMMAND='fd --follow --hidden --color=always --exclude .git'
+export FZF_DEFAULT_COMMAND='fd --follow --hidden --no-ignore --color=always --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS="--ansi"
