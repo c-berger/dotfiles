929b4a1422dd309a44363c6f33607fda66168664
C. Berger <71664347+c-berger@users.noreply.github.com>
2024-06-19 09:05:45 +0200

    Neovim: fix key mapping with Tab.

--- nvim/lua/user/plugins/codeium.lua
+++ nvim/lua/user/plugins/codeium.lua
@@ -1,6 +1,7 @@
 -- Codeium -- AI plugin
 return {
     "Exafunction/codeium.nvim",
+    build = ":Codeium Auth",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "hrsh7th/nvim-cmp",
--- nvim/lua/user/plugins/nvim-cmp.lua
+++ nvim/lua/user/plugins/nvim-cmp.lua
@@ -27,27 +27,32 @@ return {
                     end,
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
                     ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
                     ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
                     ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                    ["<Tab>"] = cmp.mapping(function() -- next expasion location
-                        if luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item() -- next suggestion
+                        elseif luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump() -- next snipped expansion location
+                        else
+                            fallback()
                         end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function() -- previous expasion location
-                        if luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
+                    end),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item() -- previous suggestion
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1) -- previous expasion location
+                        else
+                            fallback()
                         end
-                    end, { "i", "s" }),
+                    end),
                 }),
-                -- sources for autocompletion
                 sources = cmp.config.sources({
                     { name = "codeium" }, -- AI completion
                     { name = "nvim_lsp" }, -- lsp
