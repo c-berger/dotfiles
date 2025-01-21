8d5e6570d36d6130e47e848c0e37cc90cfca7318
C. Berger <71664347+c-berger@users.noreply.github.com>
2025-01-21 14:35:04 +0100

    Update Aliases and Tools on Windows.

--- install_tools.ps1
+++ install_tools.ps1
@@ -4,40 +4,43 @@ Write-Host "Set required privileges for current user"
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 
 # Install Scoop
-if ((Get-Command "scoop" -ErrorAction SilentlyContinue) -eq $null)
-{
+if ($null -eq (Get-Command "scoop" -ErrorAction SilentlyContinue)) {
     Write-Host "Install and setup Scoop" -ForegroundColor Blue
-    iwr -useb get.scoop.sh | iex
+    Invoke-WebRequest -UseBasicParsing get.scoop.sh | Invoke-Expression
     # Install Git (required for Scoop buckets)
     scoop install git
     # Add additional Scoop packages
     scoop bucket add extras
     scoop bucket add versions
-} else {
+}
+else {
     Write-Host "Skip Scoop installation" -ForegroundColor DarkGray
 }
 
 # Install Tools via Scoop
 Write-Host "Install Tools via Scoop" -ForegroundColor Blue
 scoop install gsudo
+scoop install eza                 # ls replacement and fork of exa; https://github.com/eza-community/eza
 scoop install neovim
 scoop install lazygit
 scoop install delta
-scoop install zoxide
+scoop install zoxide              # replacement for cd; https://github.com/ajeetdsouza/zoxide
 scoop install ripgrep
 scoop install gcc make cmake
 scoop install cmder
 scoop install doublecmd
-scoop isntall ditto
+scoop install ditto
+scoop install speedcrunch
+scoop install sumatrapdf
 scoop install windows-terminal-preview
 scoop install keypirinha
 scoop install joplin
 scoop install autohotkey
 scoop install everything-alpha      # require alpha for version 1.5 to include dark mode
+scoop install vscode                # sync settings with GitHub account!!
 
 # Install Pyenv and Python
-if ((Get-Command "pyenv" -ErrorAction SilentlyContinue) -eq $null)
-{
+if ($null -eq (Get-Command "pyenv" -ErrorAction SilentlyContinue)) {
     Write-Host "Install and setup Python (via pyenv)" -ForegroundColor Blue
     Invoke-WebRequest "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" | Invoke-Expression
     
@@ -49,7 +52,6 @@ if ((Get-Command "pyenv" -ErrorAction SilentlyContinue) -eq $null)
     Write-Host "USER PATH: $([System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User))" -ForegroundColor Green
     Write-Host "MACH PATH: $([System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine))" -ForegroundColor Green
     Write-Host "PROC PATH: $([System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Process))" -ForegroundColor Green
-    # ######
     
     # Install and setup Python (via pyenv)
     pyenv update
@@ -64,8 +66,12 @@ if ((Get-Command "pyenv" -ErrorAction SilentlyContinue) -eq $null)
     # # pip install robotframework pythonnet
     # 
     pyenv rehash
-} else {
+}
+else {
     Write-Host "Skip Pyenv/Python installation" -ForegroundColor DarkGray
 }
+
+# Install Ubuntu on WSL
+wsl --install -d Ubuntu
 
 Write-Host "Installed tools successfully!" -ForegroundColor Green
