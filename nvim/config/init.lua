b932fd5522b57c4cf51c1b14769b424ac954523d
C. Berger <71664347+c-berger@users.noreply.github.com>
2025-12-07 20:00:55 +0100

    Nvim: Switch to LazyVim Configuration.

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -1,15 +1,2 @@
-require("user.core.options")
-require("user.core.keymaps")
-
-if vim.g.vscode then
-    -- VSCode extension
-else
-    -- ordinary Neovim
-    require("user.core.plugins")
-
-    -- colorscheme
-    -- vim.o.background = "dark" -- or "light" for light mode
-    -- vim.cmd([[colorscheme gruvbox-baby]])
-    -- vim.cmd([[colorscheme gruvbox-baby]]) -- hack to properly load colorscheme
-    vim.cmd([[colorscheme catppuccin-mocha]])
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/config/lua/user/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tpope/vim-repeat", -- fix "." for pluging mappings
-	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-	"szw/vim-maximizer", -- maximizes and restores current window
-	"tpope/vim-surround", -- add, delete, change surroundings (it's awesome)
-	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
-	"famiu/bufdelete.nvim", -- properly delete buffers without closing windows
-	{ "numToStr/Comment.nvim", opts = {} }, -- comment/uncomment
-}
