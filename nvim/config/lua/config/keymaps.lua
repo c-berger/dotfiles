eb6d65842fea847e78aa5a1c2bdc4125e9a019bd
C. Berger <71664347+c-berger@users.noreply.github.com>
2025-12-14 23:47:30 +0100

    Nvim: Add Tmux-Navigator plugin and update configuration.

--- nvim/config/lua/config/keymaps.lua
+++ nvim/config/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Default keymaps that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--------------------------------------------------------------------------------
+
+local keymap = vim.keymap
+local function options(description)
+  return { desc = description, noremap = true, silent = true, nowait = true }
+end
+
+-- general
+
+-- do not copy character deleted with x
+keymap.set("n", "x", '"_x', options("Delete character without clipboard"))
+
+-- center cursor after jumping vertically
+keymap.set("n", "<C-d>", "<C-d>zz", options("center view after scrolling down"))
+keymap.set("n", "<C-u>", "<C-u>zz", options("center view after scolling up"))
--- nvim/config/lua/config/options.lua
+++ nvim/config/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Default options that are always set:
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--------------------------------------------------------------------------------
+
+-- Change behavior of the "Root Dir" versions to also use the current working
+-- directory instead of the LSP root directory.
+-- vim.g.root_spec = { "lsp", { "git", "lua" }, "cwd" }
+vim.g.root_spec = { "git", "cwd" }
