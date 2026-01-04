9d631b7792fd30430bfa9ee9a7fe6b14151c7619
C. Berger <71664347+c-berger@users.noreply.github.com>
2026-01-04 22:08:53 +0100

    Hyprland: update configuration (EurKey layout)

---
+++ hyprland/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! command -v pacman &>/dev/null; then
+  echo "ERROR: 'pacman' command not found!"
+  exit 1
+fi
+
+# Install Waybar dependency first
+source "$(dirname $0)/../waybar/install.sh"
+
+echo "--- Install Hyprland ---------"
+sudo pacman -S --noconfirm --needed hyprland hyprlauncher
