354df17cef8d1545bdd34f915bbc62d5da426aa1
C. Berger <71664347+c-berger@users.noreply.github.com>
2026-01-27 10:57:46 +0100

    Hyprland: add yprlock and hypridle and cleanup config

--- hyprland/config/shutdown.sh
+++
@@ -1,6 +0,0 @@
-echo shutdown >>/tmp/hyprshutdown.log
-date >>/tmp/hyprshutdown.log
-
-echo hyprshutdown >>/tmp/hyprshutdown.log
-hyprshutdown -t 'Shutting down...' --post-cmd 'systemctl poweroff >> /tmp/hyprshutdown.log 2>&1'
-echo finished >>/tmp/hyprshutdown.log
