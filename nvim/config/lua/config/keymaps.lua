c7270c39fed584c6456c5339d633a4f5174c139d
C. Berger <71664347+c-berger@users.noreply.github.com>
2026-04-02 17:37:45 +0200

    Neovim: update keymaps

--- nvim/config/lua/config/keymaps.lua
+++ nvim/config/lua/config/keymaps.lua
@@ -19,6 +19,12 @@ keymap.set("n", "x", '"_x', options("Delete character without clipboard"))
 -- center cursor after jumping vertically
 keymap.set("n", "<C-d>", "<C-d>zz", options("Center view after scrolling down."))
 keymap.set("n", "<C-u>", "<C-u>zz", options("Center view after scolling up."))
+keymap.set("n", "]]", "]]zz", options("Center view after scrolling."))
+keymap.set("n", "[[", "[[zz", options("Center view after scrolling."))
+keymap.set("n", "[]", "[]zz", options("Center view after scrolling."))
+keymap.set("n", "][", "][zz", options("Center view after scrolling."))
+keymap.set("n", "}", "}zz", options("Center view after scrolling."))
+keymap.set("n", "{", "{zz", options("Center view after scrolling."))
 
 -- quickfix next/previous
 keymap.set("n", "<M-j>", "<cmd>cnext<CR>", options("Move to next quickfix entry."))
