1db5df07bbb85baa7d100827de71b5857e724dab
C. Berger <71664347+c-berger@users.noreply.github.com>
2026-04-28 09:03:16 +0200

    Neovim: Fix clangd configuration for GNU ARM gcc.

--- nvim/config/lua/plugins/lsp.lua
+++ nvim/config/lua/plugins/lsp.lua
@@ -2,6 +2,22 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      clangd = {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders=true",
+          "--fallback-style=llvm",
+          -- "--log=verbose",
+          -- This allows clangd to query your ARM compiler for system include paths
+          "--query-driver=C:/Users/{{USERNAME}}/scoop/apps/gcc-arm-none-eabi/current/bin/arm-none-eabi-gcc.exe",
+        },
+      },
+    },
   },
   -- stylua: ignore
   keys = {
